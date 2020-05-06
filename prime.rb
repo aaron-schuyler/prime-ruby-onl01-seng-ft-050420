d0effa6e525b425dc57beeba41f80c464e11d874
Aaron Schuyler <asky28@gmail.com>
2020-05-06 20:34:40 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,19 @@
-# Add  code here!
+def prime?(n)
+  if n <= 1
+    prime = false 
+  elsif n == 2
+    prime = true
+  else
+    i = 2
+    while i < n do
+      if n % i == 0
+       prime =false
+       break
+      else
+       prime = true
+      end
+    i+=1
+    end
+  end
+  prime
+end
